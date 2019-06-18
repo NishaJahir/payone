e1946879ca2d7c0c0c7aa35d7dbc1cfd78bea869
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-06-18 13:04:00 +0530

    Update RefundDataProvider.php

--- src/Providers/Api/Request/RefundDataProvider.php
+++ src/Providers/Api/Request/RefundDataProvider.php
@@ -3,12 +3,13 @@
 namespace Payone\Providers\Api\Request;
 
 use Plenty\Modules\Order\Models\Order;
-
+use Plenty\Plugin\Log\Loggable;
 /**
  * Class RefundDataProvider
  */
 class RefundDataProvider extends DataProviderAbstract implements DataProviderOrder
 {
+    use Loggable;
     /**
      * {@inheritdoc}
      */
@@ -36,6 +37,7 @@ class RefundDataProvider extends DataProviderAbstract implements DataProviderOrd
      */
     public function getPartialRefundData($paymentCode, Order $order, Order $refund, $preAuthUniqueId)
     {
+        $this->getLogger(__METHOD__)->error('refunddata', $order);
         $requestParams = $this->getDataFromOrder($paymentCode, $order, $preAuthUniqueId);
 
         $requestParams['order'] = $this->getOrderData($refund);
